04f07ede1b002ef095dc42bf1fafb8bd5c4efcb9
pspapleng <62070039@kmitl.ac.th>
2021-11-06 10:25:17 +0700

    clear

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import MyButton from "./MyButton";
-import MyTextInput from "./MyTextInput";
-import MyErrorMessage from "./MyErrorMessage";
-import MyIconButton from "./MyIconButton";
-import MyAvatar from "./MyAvatar";
-
-export { MyButton, MyTextInput, MyErrorMessage, MyIconButton, MyAvatar };
