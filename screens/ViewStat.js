82ba4fd80562bf9d664857d8ec685bb048217309
chulalak <56313629+chayanpx@users.noreply.github.com>
2021-11-21 16:41:17 +0700

    view stat 85%

--- screens/ViewStat.js
+++ screens/ViewStat.js
@@ -1,8 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator, ScrollView } from "react-native";
 import { LineChart, ContributionGraph } from "react-native-chart-kit";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import DropDownPicker from 'react-native-dropdown-picker';
 import Firebase, { db } from "../config/Firebase";
 import { useTheme } from "react-native-paper";
 import dayjs from "dayjs";
@@ -24,43 +23,23 @@ function ViewStat() {
     const { colors } = useTheme();
 
     const [selectedMood, setSelectedMood] = useState("");
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        { label: 'Apple', value: 'apple' },
-        { label: 'Banana', value: 'banana' }
-    ]);
 
     const chartConfig = {
-        backgroundGradientFrom: "#1E2923",
-        backgroundGradientFromOpacity: 0,
-        backgroundGradientTo: "#08130D",
-        backgroundGradientToOpacity: 0.5,
-        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#ffffff",
+        backgroundGradientTo: "#ffffff",
+        color: (opacity = 0.7) => `rgba(255, 158, 177, ${opacity})`,
         strokeWidth: 2, // optional, default 3
         barPercentage: 0.5,
         useShadowColorFromDataset: false // optional
-      };
-    //for contribute graph
-    const commitsData = [
-        { date: "2021-11-02", count: 1 },
-        { date: "2021-11-03", count: 2 },
-        { date: "2021-11-04", count: 3 },
-        { date: "2021-11-05", count: 4 },
-        { date: "2021-11-06", count: 5 },
-        { date: "2021-11-30", count: 2 },
-        { date: "2021-11-31", count: 3 },
-        { date: "2021-11-01", count: 2 },
-        { date: "2021-11-02", count: 4 },
-        { date: "2021-11-05", count: 2 },
-        { date: "2021-11-30", count: 4 }
-    ];
+    };
 
     useEffect(() => {
         const currentUser = auth.currentUser;
         const unsubscribe = db
             .collection("mood")
             .where("auth_id", "==", currentUser.uid)
+            .orderBy("create_at", "asc")
             .onSnapshot(
                 {
                     includeMetadataChanges: true,
@@ -123,6 +102,10 @@ function ViewStat() {
         );
     }
 
+    console.log("moodLength: ", mood.length)
+    console.log("haveDate: ", haveDate, "haveDateLength: ", haveDate.length)
+    console.log("month: ", month, "monthLength: ", month.length)
+
     function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
     }
@@ -133,73 +116,84 @@ function ViewStat() {
     var uniqueMonth = b.filter(onlyUnique);
 
     // console.log("unique date : ", uniqueDate);
-    // console.log("unique month : ", uniqueMonth);
+    console.log(month)
+    console.log("unique month : ", uniqueMonth);
+    // console.log("mood : ", mood)
 
     var testkeepMood = [];
-    var monthly = "";
+    var labels = "";
     var avgMonthValue = 0;
-    var totalAvgMonthValue = 0;
+    var data = 0;
     var useAnnual = [];
+    var testkeeplength = 0;
 
     const forAnnualGraph = () => {
         for (let i = 0; i < uniqueMonth.length; i++) {
             testkeepMood.push(mood.filter(x => dayjs(x.create_at.toDate()).format("YYYY-MM") == uniqueMonth[i]));
         }
-        testkeepMood.map((item, index) => {
-            item.map((check, num) => {
-                monthly = dayjs(check.create_at.toDate()).format("MMMM")
-                avgMonthValue += check.value
-                totalAvgMonthValue = avgMonthValue / item.length
-                totalAvgMonthValue = totalAvgMonthValue.toFixed(3)
-                // console.log(totalAvgMonthValue.toFixed(3))
+
+        for (let i = 0; i < testkeepMood.length; i++) {
+            testkeepMood[i].map((item, index) => {
+                testkeeplength = testkeepMood[i].length
+                labels = dayjs(item.create_at.toDate()).format("MMM")
+                avgMonthValue += item.value
             })
-        })
-        useAnnual.push({ monthly, totalAvgMonthValue})
-        monthly = "";
+            data = avgMonthValue / testkeeplength
+            console.log(data, testkeeplength)
+            useAnnual.push({ labels, data })
+            labels = "";
         avgMonthValue = 0;
-        totalAvgMonthValue = 0;
+        data = 0;
+        }
+        
     }
     forAnnualGraph();
+    // console.log("testkepp : ", testkeepMood)
+    // console.log("useannual : ",useAnnual)
 
     //for annual graph
-    // var data = {};
-    // var useData = data;
-    // useAnnual.map((item, index) => {
-    //     console.log(item.monthly)
-    //     data = {
-    //         labels: item.monthly,
-    //         datasets: [
-    //           {
-    //             data: [item.totalAvgMonthValue],
-    //             color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-    //             strokeWidth: 2 // optional
-    //           }
-    //         ],
-    //       };
-    // })
-    // console.log(data)
-    
-    var dayDateMood = "";
-    var countDateMood = 0;
+    var testdata = {};
+    var forLabel = [];
+    var forData = []
+    useAnnual.map((item, index) => {
+        forLabel.push(item.labels);
+        forData.push(item.data);
+    })
+    testdata = {
+        labels: forLabel,
+        datasets: [
+          {
+            data: forData,
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+            strokeWidth: 2 // optional
+          }
+        ],
+    };
+    console.log(forLabel)
+
+    var date = "";
+    var count = 0;
     var filterDateMood = [];
     var useConGraph = [];
     const forContributeGraph = () => {
         for (let i = 0; i < uniqueDate.length; i++) {
-            filterDateMood.push(mood.filter(x => (dayjs(x.create_at.toDate()).format("YYYY-MM-DD") == uniqueDate[i])  && (x.emotion == emoodtion)));
+            filterDateMood.push(mood.filter(x => (dayjs(x.create_at.toDate()).format("YYYY-MM-DD") == uniqueDate[i]) && (x.emotion == emoodtion)));
         }
         // console.log(filterDateMood)
         filterDateMood.map((item, index) => {
             item.map((check, index) => {
-                dayDateMood = dayjs(check.create_at.toDate()).format("YYYY-MM-DD")
-                countDateMood = item.length;
+                date = dayjs(check.create_at.toDate()).format("YYYY-MM-DD")
+                count = item.length;
             })
-            useConGraph.push({dayDateMood, countDateMood})
-            dayDateMood = "";
-            countDateMood = 0;
+            if (date != '' && count != 0) {
+                useConGraph.push({ date, count })
+            }
+            date = "";
+            count = 0;
         })
     }
     forContributeGraph();
-    console.log(useConGraph)
+    // console.log(useConGraph)
 
     return (
         <View style={styles.screen}>
@@ -210,7 +204,6 @@ function ViewStat() {
                         onPress={() => {
                             setSelectedMood('Terrible Mood');
                             setEmoodtion(1);
-                            // forContributeGraph(1);
                             console.log(selectedMood)
                         }}>
                         <MaterialCommunityIcons name="emoticon-dead-outline" color={greyColor} size={38} />
@@ -220,7 +213,6 @@ function ViewStat() {
                         onPress={() => {
                             setSelectedMood('Bad Mood');
                             setEmoodtion(2);
-                            // forContributeGraph(2);
                             console.log(selectedMood)
                         }}>
                         <MaterialCommunityIcons name="emoticon-sad-outline" color={greyColor} size={38} />
@@ -230,7 +222,6 @@ function ViewStat() {
                         onPress={() => {
                             setSelectedMood('Neutral Mood');
                             setEmoodtion(3);
-                            // forContributeGraph(3);
                             console.log(selectedMood)
                         }}>
                         <MaterialCommunityIcons name="emoticon-neutral-outline" color={greyColor} size={38} />
@@ -240,7 +231,6 @@ function ViewStat() {
                         onPress={() => {
                             setSelectedMood('Good Mood');
                             setEmoodtion(4);
-                            // forContributeGraph(4);
                             console.log(selectedMood)
                         }}>
                         <MaterialCommunityIcons name="emoticon-happy-outline" color={greyColor} size={38} />
@@ -250,66 +240,48 @@ function ViewStat() {
                         onPress={() => {
                             setSelectedMood('Happy Mood');
                             setEmoodtion(5);
-                            // forContributeGraph(5);
                             console.log(selectedMood)
                         }}>
                         <MaterialCommunityIcons name="emoticon-cool-outline" color={greyColor} size={38} />
                     </TouchableOpacity>
                 </View>
                 <View style={styles.detail1}>
-                    {/* {mood.map(x => x.create_at)} */}
-                    <DropDownPicker
-                        containerStyle={{
-                            width: 100,
-                            marginLeft: '5%'
-                        }}
-                        textStyle={{
-                            color: '#4F4F4F',
-                            fontSize: 14,
-                            fontWeight: '600'
-                        }}
-                        placeholder='2021'
-                        value={value}
-                        items={items}
-                        open={open}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                    />
                     <Text style={styles.selectedMood}> {selectedMood} </Text>
                 </View>
-
                 <View>
-                    <ContributionGraph
-                        style={styles.ContributionGraph}
-                        values={useConGraph}
-                        endDate={new Date()}
-                        numDays={105}
-                        width={300}
-                        height={200}
-                        chartConfig={chartConfig}
-                    />
+                    <ScrollView
+                        style={{ flexDirection: 'row' }}
+                        horizontal={true}>
+                        <ContributionGraph
+                            style={styles.ContributionGraph}
+                            values={useConGraph}
+                            endDate={new Date()}
+                            numDays={366}
+                            width={300}
+                            height={200}
+                            chartConfig={chartConfig}
+                        />
+                    </ScrollView>
+
                 </View>
             </View>
 
             {/* lower container */}
-            {/* <View style={styles.lowerContainer}>
+            <View style={styles.lowerContainer}>
                 <View>
                     <View style={styles.detail2}>
                         <Text style={styles.Overall}>Overall</Text>
                     </View>
                     <LineChart
                         style={styles.LineChart}
-                        data={useData}
-                        width={250}
+                        data={testdata}
+                        width={300}
                         height={256}
-                        verticalLabelRotation={30}
                         chartConfig={chartConfig}
                         bezier
                     />
                 </View>
-
-            </View> */}
+            </View>
         </View>
     );
 };
@@ -318,7 +290,6 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: "center",
-        // alignItems: "center",
     },
     moodIcon: {
         flexDirection: 'row',
@@ -335,12 +306,9 @@ const styles = StyleSheet.create({
     upperContainer: {
         backgroundColor: '#ffffff',
         borderRadius: 10,
-        // marginTop: ,
         width: '90%',
         height: '50%',
         alignSelf: 'center'
-        // width: '300px',
-        // height: '262px'
     },
     lowerContainer: {
         backgroundColor: '#ffffff',
